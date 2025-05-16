4861c4eca19dff96e5b680d96fd60c76c9b9e6f7
lawrence <lshek50@stuy.edu>
2025-05-15 22:49:11 -0400

    fixed converting to bytes

--- encode.py
+++ encode.py
@@ -1,48 +1,48 @@
 import mcschematic
 
-schem = mcschematic.MCSchematic()
 
-blocks = ["minecraft:white_wool",
-          "minecraft:light_gray_wool",
-          "minecraft:gray_wool", 
-          "minecraft:black_wool", 
-          "minecraft:brown_wool",
-          "minecraft:red_wool",
-          "minecraft:orange_wool",
-          "minecraft:yellow_wool",
-          "minecraft:lime_wool",
-          "minecraft:green_wool",
-          "minecraft:cyan_wool",
-          "minecraft:light_blue_wool",
-          "minecraft:blue_wool",
-          "minecraft:purple_wool",
-          "minecraft:magenta_wool",
-          "minecraft:pink_wool"]
+bytes_to_blocks = ["minecraft:white_wool",
+                   "minecraft:light_gray_wool",
+                   "minecraft:gray_wool", 
+                   "minecraft:black_wool", 
+                   "minecraft:brown_wool",
+                   "minecraft:red_wool",  
+                   "minecraft:orange_wool",
+                   "minecraft:yellow_wool",
+                   "minecraft:lime_wool",
+                   "minecraft:green_wool",
+                   "minecraft:cyan_wool",
+                   "minecraft:light_blue_wool",
+                   "minecraft:blue_wool",
+                   "minecraft:purple_wool",
+                   "minecraft:magenta_wool",
+                   "minecraft:pink_wool"]
 
 def main():
-    file_bytes = read_bytes("test.txt");
-    bytes_list = []
-    for byte in file_bytes:
-        to_hex = hex(byte)
-        bytes_list.append(convert(to_hex[-2]))
-        bytes_list.append(convert(to_hex[-1]))
-
-    for i in bytes_list:
-        schem.setBlock((-8 + i, -1, 0), blocks[int(bytes_list[i])])
-
-    schem.save("/mnt/c/Users/lawre/AppData/Roaming/.minecraft/config/worldedit/schematics", "secret", mcschematic.Version.JE_1_21_5)
-    
+    build_schematic("test.txt")
 
 
 def read_bytes(filename):
     with open(filename, "rb") as f:
-        f_bytes = f.read()
-        return f_bytes
+        data = f.read()
+    hex_digits = []
+    for byte in data:
+        hex_digits.append(byte >> 4)     
+        hex_digits.append(byte & 0x0F)   
+    return hex_digits
 
-def convert(x):
-    if x.isnumeric():
-        return int(x)
-    return int(ord(x) - ord('a')) + 10
+
+def build_schematic(filename):
+    schem = mcschematic.MCSchematic()
+    hex_digits = read_bytes(filename)
+
+    for i, digit in enumerate(hex_digits):
+        x = i % 16
+        z = i // 16
+        block = bytes_to_blocks[digit]
+        schem.setBlock((x, 0, z), block)
+
+    schem.save("/mnt/c/Users/lawre/AppData/Roaming/.minecraft/config/worldedit/schematics", "secret", mcschematic.Version.JE_1_21_5)
 
 if __name__ == "__main__":
     main()
