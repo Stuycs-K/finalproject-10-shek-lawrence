8c25fc8843a0a3ac6a551235f847e146567eeda0
lawrence <lshek50@stuy.edu>
2025-05-16 23:16:12 -0400

    read blocks from world working

--- encode.py
+++ encode.py
@@ -1,7 +1,10 @@
 import mcschematic
+import nbtlib 
+
+SCHEMATICS_FOLDER = "/mnt/c/Users/lawre/AppData/Roaming/.minecraft/config/worldedit/schematics/"
 
 
-bytes_to_blocks = ["minecraft:white_wool",
+BYTES_TO_BLOCKS = ["minecraft:white_wool",
                    "minecraft:light_gray_wool",
                    "minecraft:gray_wool", 
                    "minecraft:black_wool", 
@@ -18,8 +21,14 @@ bytes_to_blocks = ["minecraft:white_wool",
                    "minecraft:magenta_wool",
                    "minecraft:pink_wool"]
 
+BLOCKS_TO_BYTES =  {block: i for i, block in enumerate(BYTES_TO_BLOCKS)}
+
+
 def main():
     build_schematic("test.txt")
+    decoded_bytes = decode_schematic(SCHEMATICS_FOLDER + "encoded.schem")
+    # with open("decrypted.txt", "wb") as f:
+        # f.write(decoded_bytes)
 
 
 def read_bytes(filename):
@@ -39,10 +48,27 @@ def build_schematic(filename):
     for i, digit in enumerate(hex_digits):
         x = i % 16
         z = i // 16
-        block = bytes_to_blocks[digit]
+        block = BYTES_TO_BLOCKS[digit]
         schem.setBlock((x, 0, z), block)
 
-    schem.save("/mnt/c/Users/lawre/AppData/Roaming/.minecraft/config/worldedit/schematics", "secret", mcschematic.Version.JE_1_21_5)
+    schem.save(SCHEMATICS_FOLDER, "secret", mcschematic.Version.JE_1_21_5)
+
+
+def decode_schematic(filepath):
+    schem = nbtlib.load(filepath)
+    # palette is a dictionary that maps each block to an integer 
+    palette = schem['Schematic']['Blocks']['Palette']
+    # data is a byte array storing the order that the blocks are stored, using the indices in palette corresponding to the name of the block
+    data = schem['Schematic']['Blocks']['Data']
+    
+    index_to_block = {v: k for k, v in palette.items()}
+    ordered_blocks = [index_to_block[byte] for byte in data]
+    for i, block in enumerate(ordered_blocks):
+        print(f"{i}: {block}")
+
+
+    return
+
 
 if __name__ == "__main__":
     main()
