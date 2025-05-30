f0214c676a350bb913606731b89282399dd00205
lawrence <lshek50@stuy.edu>
2025-05-30 15:25:02 -0400

    decode shuffled positions (working sometimes?)

--- v2.py
+++ v2.py
@@ -33,15 +33,16 @@ with open("blocks.txt", "r") as f:
         BYTES_TO_BLOCKS.append(f.readline().rstrip())
 
 
-TOTAL_BYTES = 0
 SEED = 7114238357002984737
+
+# first 8 blocks of the file store the file size
+FILE_START = 8
 
 def main():
     parser = argparse.ArgumentParser(description="Encode a file or decode a schematic file.")
     parser.add_argument("-m", "--mode", choices=["encode", "decode"], type=str.lower, help="Mode of operation")
     parser.add_argument("-i", "--input", help="Name of the file to process") 
     parser.add_argument("-o", "--output", help="Output file name")
-    parser.add_argument("-b", "--bytes", type=int, help="Number of bytes")
 
 
     args = parser.parse_args()
@@ -52,8 +53,6 @@ def main():
     elif args.mode == "decode":
         if args.output is None:
             args.output = "decrypted.txt"
-        global TOTAL_BYTES
-        TOTAL_BYTES = args.bytes 
         decode_schematic(SCHEMATICS_FOLDER + args.input, args.output)
     else:
         print("invalid mode")
@@ -77,7 +76,7 @@ def read_bytes(filename):
 
     # prepend length of file in first 8 blocks (32 bit integer)
     file_size = len(data)
-    for i in range(8):
+    for i in range(FILE_START):
         hex_digits.append(file_size & 0x0F)
         file_size = file_size >> 4
     
@@ -91,8 +90,6 @@ def read_bytes(filename):
 
         hex_digits.append(byte & 0x0F)   
 
-
-    print("number of bytes", str(len(data) + additional_bytes))
     return hex_digits
 
 
@@ -112,12 +109,9 @@ def build_schematic(filename, schematic_name):
     pos = shuffle_pos(side_length, side_length, side_length, SEED)
     for i, val in enumerate(hex_digits):
         x, y, z = pos[i]
-        # x = i % side_length
-        # z = (i // side_length) % side_length
-        # y = i // (side_length * side_length)
         block = BYTES_TO_BLOCKS[val]
         schem.setBlock((x, y, z), block)
-
+    
     schem.save(SCHEMATICS_FOLDER, schematic_name, mcschematic.Version.JE_1_21_5)
 
 
@@ -150,30 +144,24 @@ def decode_schematic(filepath, output_file):
     pos_to_index = {pos : i for i, pos in enumerate(block_positions)}     
 
     
-    # convert values from block array to byte array 
-    byte_array = []
-
-    for pos in block_positions:
-        index = pos_to_index[pos]
-        val = data[index] % 256
-        block = val_to_block[val]
-        if block in BLOCKS_TO_BYTES:
-            byte_array.append(BLOCKS_TO_BYTES[block])
-        
-
-    print("bytes " + str(len(byte_array)))
-    print("total blocks " + str(len(block_positions)))
-    # for byte in data[:TOTAL_BYTES * 2]:
-    #     # Byte objects in the data array are signed integers from -128 to 127, but the array is indexed 0-255
-    #     byte = byte % 256
-    #     block = value_to_block[byte]
-    #     # don't include extranneous blocks
-    #     if block in BLOCKS_TO_BYTES:
-    #         byte_array.append(BLOCKS_TO_BYTES[block])
+    byte_array = [None] * len(block_positions)
     
+    # convert values from block array to byte array 
+    for i, byte in enumerate(data):
+        # Byte objects in the data array are signed integers from -128 to 127, but the array is indexed 0-255
+        block = val_to_block[byte % 256]
+        pos = get_pos(i, width, height, length)
+        index = pos_to_index[pos]
+        if block in BLOCKS_TO_BYTES:
+            byte_array[index] = BLOCKS_TO_BYTES[block]
+
+
+    total_bytes = get_file_size(byte_array[:FILE_START])
+
+
     # convert back to byte values
     temp = []
-    for i in range(0, len(byte_array), 2):
+    for i in range(FILE_START, FILE_START + total_bytes * 2, 2):
         temp.append(16 * byte_array[i] + byte_array[i + 1])
     byte_array = temp
 
@@ -203,9 +191,10 @@ def shuffle_pos(width, height, length, seed):
 
 
 def get_pos(index, width, height, length):
+    # blocks in the schematic are read in the order: x, z, y (all in the positive direction)
     y = index // (width * length)
     rem = index % (width * length)
-    z = rem // width
+    z = rem // length
     x = rem % width
     return (x, y, z)
 
